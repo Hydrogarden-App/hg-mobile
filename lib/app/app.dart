d6cd7b061eaec5007f7a5856b28cf5ab3a1a6391
Anna Sidor <annasidor00@gmail.com>
2026-02-02 21:06:20 +0100

    fix: upgrade and downgrade some deps

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,4 +1,4 @@
-// Openapi Generator last run: : 2025-09-14T20:24:32.489612
+// Openapi Generator last run: : 2026-02-02T21:01:58.881287
 import "package:connectivity_plus/connectivity_plus.dart";
 import "package:flutter/material.dart";
 import "package:flutter_bloc/flutter_bloc.dart";
